6efa21dc4adfcd17d7ce61f3ece2ea08b3648a64
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-03-27 21:23:33 +0000

    Failure Heatmap Data Graphs
    - Created Heatmap Graphs for failures
    - This is because I think this is why I'm seeing strange trends in block Size
    - Some graphs are logarithmic

--- Adaptive Embedding Idea/HeatmapMaker.py
+++ Adaptive Embedding Idea/HeatmapMaker.py
@@ -6,6 +6,7 @@ from math import ceil
 from datetime import datetime
 import matplotlib.pyplot as plt
 from Helpers.HelperFunctions import *
+from matplotlib.colors import LogNorm
 from Helpers.SteganalysisMethods import *
 from Helpers.EmbeddingAlgorithms import *
 from Helpers.TestingScriptFuncVersion import CompositeMethod
@@ -22,7 +23,7 @@ DEVIATION_COEFFICENTS = {
     "Zhang" : -1/10856.838
 } 
 
-def GenerateBlockSizeHeatmapData(isIndexBlockMethod : bool):
+def GenerateBlockSizeHeatmapData(isIndexBlockMethod : bool, thresholdArg : float):
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
     cursor.execute("""
@@ -73,7 +74,7 @@ def GenerateBlockSizeHeatmapData(isIndexBlockMethod : bool):
                 if(index % 10 == 0):
                     print(f"{datetime.now().strftime("%H:%M:%S")} - Index : {index}")
                 
-                stegoBlock, _, _ = CompositeMethod(embedData[:secretBytesAmount], os.path.join(BOSSBASE_FOLDER, path), acceptableMappingThreshold=100, bytesPerSection=bytesPerSection, blocksPerSide=int(256 / blockSize[0]), indexBlockMethod=isIndexBlockMethod)
+                stegoBlock, _, _ = CompositeMethod(embedData[:secretBytesAmount], os.path.join(BOSSBASE_FOLDER, path), acceptableMappingThreshold=thresholdArg, bytesPerSection=bytesPerSection, blocksPerSide=int(256 / blockSize[0]), indexBlockMethod=isIndexBlockMethod)
                 
                 consideredBlock = SplitIntoBlocks(os.path.join(BOSSBASE_FOLDER, path), 256)[0][0]
                 coverMappings = {
@@ -168,6 +169,93 @@ def HeatmapDisplay():
     #plt.show()
     plt.tight_layout()
     plt.savefig(f"Block Size VS Threshold Composite Alpha Heatmap 1000 Mark Method.png", dpi=600)
+    
+    #*Failure data
+    
+    #Block Size Data
+    df = pd.read_sql_query("SELECT * FROM CompositeHeatmapBlockSizeFailures WHERE BlockSize != 256 AND IsIndexBlockMethod = 1 ", conn) 
+    heatmapData = df.pivot( index="BlockSize", columns="EmbedPercentage", values="Failures" ) 
+    annotLabels = heatmapData.map(lambda x: f"{x:.3g}")
+    plt.figure(figsize=(12, 5))
+    sns.heatmap(
+        heatmapData,
+        cmap="YlOrRd",
+        annot=annotLabels,
+        fmt=""
+    )
+    plt.xlabel("Embedding Percentage (%)")
+    plt.ylabel("Block Size")
+    plt.title(f"Heatmap Of No. Of Failures For Block Size Against Embed % For First 1000 Images Of BossBase Using Index Block Method")
+    #plt.show()
+    plt.tight_layout()
+    plt.savefig(f"Block Size VS Embedding Rate Composite Failure Heatmap 1000 Index Method.png", dpi=600)
+    
+    
+    df = pd.read_sql_query("SELECT * FROM CompositeHeatmapBlockSizeFailures WHERE BlockSize != 256 AND IsIndexBlockMethod = 0 ", conn) 
+    heatmapData = df.pivot( index="BlockSize", columns="EmbedPercentage", values="Failures" ) 
+    annotLabels = heatmapData.map(lambda x: f"{x:.3g}")
+    plt.figure(figsize=(12, 5))
+    sns.heatmap(
+        heatmapData,
+        cmap="YlOrRd",
+        annot=annotLabels,
+        fmt=""
+    )
+    plt.xlabel("Embedding Percentage (%)")
+    plt.ylabel("Block Size")
+    plt.title(f"Heatmap Of α For Block Size Against Embed % For First 1000 Images Of BossBase Using Block Marking Method")
+    #plt.show()
+    plt.tight_layout()
+    plt.savefig(f"Block Size VS Embedding Rate Composite Failure Heatmap 1000 Mark Method.png", dpi=600)
+ 
+    #Threshold Data
+    df = pd.read_sql_query("SELECT * FROM CompositeHeatmapThresholdFailures WHERE IsIndexBlockMethod = 1", conn) 
+    heatmapData = df.pivot( index="Threshold", columns="EmbedPercentage", values="Failures" ) 
+    annotLabels = heatmapData.map(lambda x: f"{x:.3g}")
+    plt.figure(figsize=(12, 5))
+    
+    #!LOG STUFF
+    heatmapData = heatmapData.replace(0, 1e-6)
+    sns.heatmap(
+        heatmapData,
+        cmap="YlOrRd",
+        annot=annotLabels,
+        fmt="",
+        norm=LogNorm(
+            vmin=heatmapData[heatmapData > 0].min().min(),  # smallest non-zero
+            vmax=heatmapData.max().max()
+        )
+    )
+    plt.xlabel("Embedding Percentage (%)")
+    plt.ylabel("Security Threshold [α_t]")
+    plt.title(f"Heatmap Of Failures For Threshold (α_t) Against Embed % For First 1000 Images Of BossBase Using Index Block Method")
+    #plt.show()
+    plt.tight_layout()
+    plt.savefig(f"Threshold VS Embedding Rate Composite Failure Heatmap 1000 Index Method.png", dpi=600)
+
+    df = pd.read_sql_query("SELECT * FROM CompositeHeatmapThresholdFailures WHERE IsIndexBlockMethod = 0", conn) 
+    heatmapData = df.pivot( index="Threshold", columns="EmbedPercentage", values="Failures" ) 
+    annotLabels = heatmapData.map(lambda x: f"{x:.3g}")
+    plt.figure(figsize=(12, 5))
+    #!LOG STUFF
+    heatmapData = heatmapData.replace(0, 1e-6)
+    sns.heatmap(
+        heatmapData,
+        cmap="YlOrRd",
+        annot=annotLabels,
+        fmt="",
+        norm=LogNorm(
+            vmin=heatmapData[heatmapData > 0].min().min(),  # smallest non-zero
+            vmax=heatmapData.max().max()
+        )
+    )
+    
+    plt.xlabel("Embedding Percentage (%)")
+    plt.ylabel("Security Threshold [α_t]")
+    plt.title(f"Heatmap Of Failures For Threshold (α_t) Against Embed % For First 1000 Images Of BossBase Using Block Marking Method")
+    #plt.show()
+    plt.tight_layout()
+    plt.savefig(f"Threshold vs Embedding Rate Composite Failures Heatmap 1000 Mark Method.png", dpi=600)
 
 def GenerateThresholdHeatmapData(isIndexBlockMethod : bool, blockSize : tuple[int, int] = (64, 64)):
     conn = sqlite3.connect(DATABASE_NAME)
@@ -244,13 +332,160 @@ def GenerateThresholdHeatmapData(isIndexBlockMethod : bool, blockSize : tuple[in
             
     conn.close()
 
+def GenerateThresholdFailureHeatmapData(isIndexBlockMethod : bool, blockSize : tuple[int, int] = (64, 64)):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CompositeHeatmapThresholdFailures
+    (
+        EmbedPercentage FLOAT NOT NULL,
+        Threshold FLOAT NOT NULL,
+        Failures FLOAT NOT NULL,
+        IsIndexBlockMethod INTEGER NOT NULL
+    )""")
+    conn.commit()
+
+    coverBlocks = []
+    for path in paths:
+        block = SplitIntoBlocks(os.path.join(BOSSBASE_FOLDER, path), 256)[0][0]
+        coverBlocks.append(block)
+
+    embedData = []
+    with open("Lipsum.txt", "r") as f:
+        embedData = [*f.read()]
+        
+        
+    #Finding amount of bytes to embed
+
+    for k in range(25,225,25):
+        i = k / 2
+        totalImageBytes = (imageSize[0] * imageSize[1]) / 8
+        secretBytesAmount = int(totalImageBytes * (i/100))
+
+        for l in range(1, 11):
+            j = l / 2
+
+            #Seeing if its already in the SQL - If it is, we skip to the next one
+            cursor.execute("SELECT * FROM CompositeHeatmapThresholdFailures WHERE EmbedPercentage = ? AND Threshold = ? AND isIndexBlockMethod = ?", (i, j, int(isIndexBlockMethod))) 
+            rows = cursor.fetchone()
+            if not(rows == None or rows == []):
+                continue
+
+            #Finding the bytes per section to attempt
+            bytesPerSection = ceil(secretBytesAmount / ((imageSize[0] / blockSize[0]) ** 2))
+            
+            print(f"Secret Bytes Amount : {secretBytesAmount} | Bytes Per Section : {bytesPerSection}")
+            
+            index = 0
+
+            totalFailureSum = 0
+            for path in paths:
+                
+                if(index % 10 == 0):
+                    print(f"{datetime.now().strftime("%H:%M:%S")} - Index : {index}")
+                
+                _, blockCountData, _ = CompositeMethod(embedData[:secretBytesAmount], os.path.join(BOSSBASE_FOLDER, path), acceptableMappingThreshold=j, bytesPerSection=bytesPerSection, blocksPerSide=int(256 / blockSize[0]), indexBlockMethod=isIndexBlockMethod)
+                
+                totalFailureSum += blockCountData[0]
+                index += 1
+            
+            totalFailureSum /= index
+            cursor.execute("""INSERT INTO CompositeHeatmapThresholdFailures
+            (EmbedPercentage, Threshold, Failures, IsIndexBlockMethod)
+            VALUES (?, ?, ?, ?)""",(i, j, totalFailureSum, int(isIndexBlockMethod)))
+            conn.commit()
+            print(f"{datetime.now().strftime("%H:%M:%S")} - {i}|{j} complete")
+            
+    conn.close()
+
+def GenerateBlockSizeFailureHeatmapData(isIndexBlockMethod : bool, thresholdArg : float):
+    conn = sqlite3.connect(DATABASE_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CompositeHeatmapBlockSizeFailures
+    (
+        EmbedPercentage FLOAT NOT NULL,
+        BlockSize INT NOT NULL,
+        Failures FLOAT NOT NULL,
+        IsIndexBlockMethod INTEGER NOT NULL
+    )""")
+    conn.commit()
+
+    coverBlocks = []
+    for path in paths:
+        block = SplitIntoBlocks(os.path.join(BOSSBASE_FOLDER, path), 256)[0][0]
+        coverBlocks.append(block)
+
+    embedData = []
+    with open("Lipsum.txt", "r") as f:
+        embedData = [*f.read()]
+        
+        
+    #Finding amount of bytes to embed
+
+    for i in range(25,101,25):
+        totalImageBytes = (imageSize[0] * imageSize[1]) / 8
+        secretBytesAmount = int(totalImageBytes * (i/100))
+
+        #!TEMP - CAHNGE THIS FOR DIFFERENT SIZES
+        for j in range(3,8):
+            blockSize = (2 ** j, 2**j)
+
+            cursor.execute("SELECT * FROM CompositeHeatmapBlockSizeFailures WHERE EmbedPercentage = ? AND BlockSize = ? AND IsIndexBlockMethod = ?", (i, blockSize[0], int(isIndexBlockMethod))) 
+            rows = cursor.fetchone()
+            if not(rows == None or rows == []):
+                continue
+            
+            #Finding the bytes per section to attempt
+            bytesPerSection = ceil(secretBytesAmount / ((imageSize[0] / blockSize[0]) ** 2))
+            
+            print(f"Secret Bytes Amount : {secretBytesAmount} | Bytes Per Section : {bytesPerSection}")
+            
+            index = 0
+
+            totalFailureSum = 0
+            for path in paths:
+                
+                if(index % 10 == 0):
+                    print(f"{datetime.now().strftime("%H:%M:%S")} - Index : {index}")
+                
+                _, blockCountData, _ = CompositeMethod(embedData[:secretBytesAmount], os.path.join(BOSSBASE_FOLDER, path), acceptableMappingThreshold=thresholdArg, bytesPerSection=bytesPerSection, blocksPerSide=int(256 / blockSize[0]), indexBlockMethod=isIndexBlockMethod)
+                
+                
+                totalFailureSum += blockCountData[0]
+                index += 1
+            
+            totalFailureSum /= index
+            cursor.execute("""INSERT INTO CompositeHeatmapBlockSizeFailures
+            (EmbedPercentage, BlockSize, Failures, IsIndexBlockMethod)
+            VALUES (?, ?, ?, ?)""",(i, blockSize[0], totalFailureSum, int(isIndexBlockMethod)))
+            conn.commit()
+            print(f"{datetime.now().strftime("%H:%M:%S")} - {i}|{j} complete")
+            
+    conn.close()
+
 HeatmapDisplay()
-#GenerateBlockSizeHeatmapData(False)
+#GenerateBlockSizeHeatmapData(False, 3)
 print("1 done")
-#GenerateBlockSizeHeatmapData(True)
+#GenerateBlockSizeHeatmapData(True, 3)
 print("2 done")
 #GenerateThresholdHeatmapData(False, (64,64))
 
 print("3 complete")
 
 #GenerateThresholdHeatmapData(True, (64,64))
+
+print("4 Complete")
+#GenerateThresholdFailureHeatmapData(True, (64,64))
+
+print("5 complete")
+
+#GenerateThresholdFailureHeatmapData(False, (64,64))
+
+print("6 complete")
+
+#GenerateBlockSizeFailureHeatmapData(True, 3)
+
+print("7 complete")
+
+#GenerateBlockSizeFailureHeatmapData(False, 3)
